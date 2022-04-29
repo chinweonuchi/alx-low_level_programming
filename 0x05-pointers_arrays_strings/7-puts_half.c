ae566d2a0aba1fcfb0e9faceff2d3ced1716740c
chinweonuchi <67050057+chinweonuchi@users.noreply.github.com>
2022-04-29 21:10:05 +0100

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,34 +1,30 @@
-​#​include​ ​"​main.h​" 
- ​/*​* 
- ​ * puts_half - prints a string, followed by a new line, 
- ​ * @str: pointer to the string to print 
- ​ * Return: void 
- ​*/ 
-  
- ​void​ ​puts_half​(​char​ *str) 
- ​{ 
- ​        ​int​ len, n, i; 
-  
- ​        len = ​0​; 
-  
- ​        ​while​ (str[len] != ​'​\0​'​) 
- ​        { 
- ​                len++; 
- ​        } 
-  
- ​        ​if​ (len % ​2​ == ​0​) 
- ​        { 
- ​                ​for​ (i = len / ​2​; str[i] != ​'​\0​'​; i++) 
- ​                { 
- ​                        ​_putchar​(str[i]); 
- ​                } 
- ​        } ​else​ ​if​ (len % ​2​) 
- ​        { 
- ​                ​for​ (n = (len - ​1​) / ​2​; n < len - ​1​; n++) 
- ​                { 
- ​                        ​_putchar​(str[n + ​1​]); 
- ​                } 
- ​        } 
- ​        ​_putchar​(​'​\n​'​); 
- ​}
+#include "main.h"
 
+/**
+ *puts_half - prints every other character of a string
+ *@str: A pointer to an int that will be changed
+ *
+ *Return: void which means our answer is correct
+ */
+
+void puts_half(char *str)
+
+{
+int i, last;
+
+i = 0;
+while (str[i] != '\0')
+{
+i++;
+}
+
+last = (i + 1) / 2;
+
+for (i = last; str[i]; i++)
+{
+_putchar (str[i]);
+}
+
+_putchar ('\n');
+
+}
