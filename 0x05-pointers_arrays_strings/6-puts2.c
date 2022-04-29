da693950cb7fbc3e7965fc789c724780fc177a73
chinweonuchi <67050057+chinweonuchi@users.noreply.github.com>
2022-04-29 21:09:15 +0100

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,22 +1,21 @@
-#​include​ ​"​main.h​" 
- ​/*​* 
- ​ * puts2 - prints a string, followed by a new line, 
- ​ * @str: pointer to the string to print 
- ​ * Return: void 
- ​*/ 
-  
- ​void​ ​puts2​(​char​ *str) 
- ​{ 
- ​        ​int​ i = ​0​; 
-  
- ​        ​while​ (str[i] != ​'​\0​'​) 
- ​        { 
- ​                ​if​ (i % ​2​ == ​0​) 
- ​                { 
- ​                        ​_putchar​(str[i]); 
- ​                } 
-  
- ​                i++; 
- ​        } 
- ​        ​_putchar​(​'​\n​'​); 
- ​}
+#include "main.h"
+
+/**
+ *puts2 - prints every character of a string
+ *@str: A pointer to an int that will be changed
+ *
+ *Return: void which means it is correct
+ */
+
+void puts2(char *str)
+{
+int a;
+
+for (a = 0; str[a] != '\0'; a++)
+{
+if (a % 2 == 0)
+_putchar (str[a]);
+}
+
+_putchar ('\n');
+}
